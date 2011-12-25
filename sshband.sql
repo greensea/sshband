950e183cd57efe76c5b87d6a2e0ed4708df851b6
gs@bbxy.net <gs@bbxy.net@302af1a5-2c98-5b97-2cd7-40ce592a1a8b>
2011-12-25 06:51:48 +0000

    增加默认 MySQL 数据表结构

---
+++ sshband.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `sshacct` (
+  `id` int(11) NOT NULL auto_increment,
+  `uid` int(11) NOT NULL,
+  `inband` bigint(20) NOT NULL,
+  `outband` bigint(20) NOT NULL,
+  `connecttime` datetime NOT NULL,
+  `disconnecttime` datetime NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `clientip` char(15) NOT NULL,
+  `sessionid` char(32) NOT NULL,
+  `clientport` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
