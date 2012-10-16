3fd02fddf7e3c396c6d2aa504c89dfafcb383eec
gs@bbxy.net <gs@bbxy.net@302af1a5-2c98-5b97-2cd7-40ce592a1a8b>
2012-10-16 07:04:22 +0000

    * 去掉在 64 位平台上关于 printf 数据类型的编译告警

--- sshband.h
+++ sshband.h
@@ -47,8 +47,8 @@ typedef struct ssh_session_t {
 	uid_t uid;
 	u_long client_data_time;	// 最后一次收到客户端数据包的时间
 	u_long stime;	// 会话启动时间
-	uint64_t outband;	// 服务器端上行流量
-	uint64_t inband;	// 服务器下行流量
+	unsigned long long outband;	// 服务器端上行流量
+	unsigned long long inband;	// 服务器下行流量
 	char client_addr[17];	// 客户端地址
 	char sessid[33];	// 会话唯一标识
 	struct in_addr ip;
